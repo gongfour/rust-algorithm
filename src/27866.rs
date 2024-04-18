e124c0fd421622af9b6d483c90b4a81c2f90b03d
wonjin <kangwonjin.dev@gmail.com>
2024-04-18 23:23:21 +0900

    solve 27866

---
+++ src/27866.rs
@@ -0,0 +1,13 @@
+use std::io::{self};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let string: String = input.parse().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+    let idx: usize = input.parse().unwrap();
+
+    println!("{}", string.chars().nth(idx - 1).unwrap());
+}
